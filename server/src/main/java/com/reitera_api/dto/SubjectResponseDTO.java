f6b9b868d96a6a72b65eee83a370e8da1d58ac06
Sylvarr <whitessx@gmail.com>
2026-06-01 20:22:06 +0200

    Converted SubjectResponseDTO into record.

--- server/src/main/java/com/reitera_api/dto/SubjectResponseDTO.java
+++ server/src/main/java/com/reitera_api/dto/SubjectResponseDTO.java
@@ -1,32 +1,5 @@
 package com.reitera_api.dto;
 
-public class SubjectResponseDTO {
+public record SubjectResponseDTO(Long id, String name, String color, int total_topics) {
 
-    private Long id;
-    private String name;
-    private String color;
-    private int total_topics;
-
-    public SubjectResponseDTO(Long id, String name, String color, int total_topics) {
-        this.id = id;
-        this.name = name;
-        this.color = color;
-        this.total_topics = total_topics;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
-    public int getTotal_topics() {
-        return total_topics;
-    }
 }
